d0177409cae75887349b25cb2e122e219aa4d433
luca <rencord@gmail.com>
2017-08-14 13:11:31 +0200

    Comment Entity class

--- src/entity/Comment.java
+++ src/entity/Comment.java
@@ -4,4 +4,32 @@ public class Comment {
 	public String content;
 	public User owner;
 	public Post attachedPost;
+	public int likes;
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public User getOwner() {
+		return owner;
+	}
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	public Post getAttachedPost() {
+		return attachedPost;
+	}
+	public void setAttachedPost(Post attachedPost) {
+		this.attachedPost = attachedPost;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	} 
+	
+	
+	
 }
